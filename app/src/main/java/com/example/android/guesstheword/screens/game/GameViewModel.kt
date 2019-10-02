9234a0435e376c7f46c71baa5cbdfc5aac131796
wittayakaew <59160039@go.buu.ac.th>
2019-10-02 18:37:50 +0700

    finish add timer

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,8 +1,11 @@
 package com.example.android.guesstheword.screens.game
 
+import android.os.CountDownTimer
+import android.text.format.DateUtils
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 
 class GameViewModel :ViewModel(){
@@ -17,14 +20,43 @@ class GameViewModel :ViewModel(){
     private val _eventGameFinish= MutableLiveData<Boolean>()
     val evenGameFinish : LiveData<Boolean>
         get() = _eventGameFinish
+
+    private val _currentTime = MutableLiveData<Long>()
+    val currentTime :LiveData<Long>
+    get() = _currentTime
+
+    val currentTimeString = Transformations.map(currentTime){
+        time->
+        DateUtils.formatElapsedTime(time)
+    }
+    private val timer : CountDownTimer
+
+
     init {
         Log.i("GameViewModel", "GameViewModel created!")
         _word.value = ""
         _score.value = 0
+        timer = object :CountDownTimer(COUNTDONW_TIME, ONE_SECOND){
+            override fun onFinish() {
+                 _currentTime.value = DONE
+            }
+
+            override fun onTick(millisUntilFinished: Long) {
+                _currentTime.value = millisUntilFinished/ ONE_SECOND
+            }
+
+        }
+        timer.start()
+    }
+    companion object{
+        private const val DONE = 0L
+        private const val ONE_SECOND = 1000L
+        private const val COUNTDONW_TIME = 60000L
     }
     override fun onCleared() {
         super.onCleared()
         Log.i("GameViewModel", "GameViewModel destroyed!")
+        timer.cancel()
     }
     // The current _word
 
@@ -72,7 +104,7 @@ class GameViewModel :ViewModel(){
     }
    fun nextWord() {
         if (wordList.isEmpty()) {
-           onGameFinish()
+         resetList()
         }else{
             //Select and remove a _word from the list
             _word.value = wordList.removeAt(0)
